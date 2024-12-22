0a69d5d23b6cbeef5a03aa552610bf805cb296d1
rbisht03 <riyabisht979@gmail.com>
2024-12-22 16:58:00 +0530

    Duplicate entry

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,15 +2,17 @@ import java.util.*;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
-//    ArrayList<Contacts>adbook1=new ArrayList<>();
-    Map<String, List<Contacts>> ad=new HashMap<>();
+
+
+    Map<String, Set<Contacts>> ad=new HashMap<>();
+
+
     public void CreateContact() {
         System.out.println("Enter the name of AddressBook where you want to create contact: ");
         String book=sc.next();
-        List<Contacts>a1=ad.get(book);
+        Set<Contacts>a1=ad.get(book);
         if(a1==null){
-            System.out.println("No such addressBook");
-            return;
+            System.out.println("No such addressBook");return;
         }
         System.out.println("Enter first name:");
         String f_name = sc.next();
@@ -37,14 +39,16 @@ public class AddressBook {
     public void editContact() {
         System.out.println("Enter the name of AddressBook where you want to create contact: ");
         String book=sc.next();
-        List<Contacts>a1=ad.get(book);
+        Set<Contacts>a1=ad.get(book);
         if(a1==null){
-            System.out.println("No such addressBook");
-            return;
+            System.out.println("No such addressBook");return;
         }
         System.out.println("Enter the first name of person whose detail you want to edit:");
-        String name=sc.next();
-        a1.stream().filter(t->t.getFirstName().equalsIgnoreCase(name))
+        String fname=sc.next();
+        System.out.println("Enter the last name of person whose detail you want to delete:");
+        String lname=sc.next();
+
+        a1.stream().filter(t->t.getFirstName().equalsIgnoreCase(fname) && t.getlastName().equalsIgnoreCase(lname))
                 .map(t->{
                     System.out.println("Contact Found");
                     System.out.println("What would you like to edit? Press 1) first name \\n2) for last name \\n 3) for city \\n 4) for state \\n 5) for email \\n 6)Phone \\n 7) zip code");
@@ -62,13 +66,13 @@ public class AddressBook {
                     System.out.println("Contact Updated");
                     return true;
                 }).findFirst().orElse(null);
-        return;
     }
+
 
     public void display(){
         System.out.println("Enter the name of AddressBook where you want to create contact: ");
         String book=sc.next();
-        List<Contacts>a1=ad.get(book);
+        Set<Contacts>a1=ad.get(book);
         if(a1==null || a1.isEmpty()){
             System.out.println("NO Contact found in Addressbook:"+book);
             return;
@@ -77,17 +81,21 @@ public class AddressBook {
         System.out.println(a1);
     }
 
+
     public void deleteContact() {
         System.out.println("Enter the name of AddressBook where you want to create contact: ");
         String book=sc.next();
-        List<Contacts>a1=ad.get(book);
+        Set<Contacts>a1=ad.get(book);
         if(a1==null){
             System.out.println("No such addressBook");
             return;
         }
         System.out.println("Enter the first name of person whose detail you want to delete:");
-        String name=sc.next();
-        Contacts c=a1.stream().filter(t->t.getFirstName().equalsIgnoreCase(name)).findFirst().get();
+        String fname=sc.next();
+        System.out.println("Enter the last name of person whose detail you want to delete:");
+        String lname=sc.next();
+
+        Contacts c=a1.stream().filter(t->t.getFirstName().equalsIgnoreCase(fname) && t.getlastName().equalsIgnoreCase(lname) ).findFirst().get();
         if(c==null){
             System.out.println("Contact not found");
         }
@@ -97,8 +105,8 @@ public class AddressBook {
         }
     }
 
-    public void addAddressbook() {
 
+    public void addAddressbook() {
         System.out.println("Enter Adressbook Name:");
         String name=sc.next();
 
@@ -106,8 +114,10 @@ public class AddressBook {
             System.out.println("AddressBook :"+name+" already exists.");
         }
         else{
-            ad.put(name,new ArrayList<>());
+            ad.put(name,new HashSet<>());
             System.out.println("AddressBook :"+name+" added.");
         }
     }
+
+
 }
--- src/Contacts.java
+++ src/Contacts.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Contacts {
     String f_name;
     String l_name;
@@ -20,57 +22,35 @@ public class Contacts {
     }
 
 
-    public String getFirstName() {
-        return f_name;
-    }
-    public String getlastName() {
-        return l_name;
-    }
-    public String getcity() {
-        return city;
-    }
-    public String getstate() {
-        return state;
-    }
-    public String getemail() {
-        return email;
-    }
-    public int getPhonenumber() {
-        return phn_no;
-    }
-    public int zip() {
-        return zip;
+    public String getFirstName() {return f_name;}
+    public String getlastName() {return l_name;}
+    public String getcity() {return city;}
+    public String getstate() {return state;}
+    public String getemail() {return email;}
+    public int getPhonenumber() {return phn_no;}
+    public int zip() {return zip;}
+    public void setFirstname(String next) {this.f_name=next;}
+    public void setLastname(String next) {this.l_name=next;}
+    public void setCity(String next) {this.city=next;}
+    public void setState(String next) {this.state=next;}
+    public void setEmail(String next) {this.email=next;}
+    public void setPhone(int i) {this.phn_no=i;}
+    public void setZip(int i) {this.zip=i;}
+    public String toString() {return "["+f_name+" "+l_name+" " +address+" "+city+" "+state+" "+zip+" "+phn_no+" "+email+"]";}
+
+
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if(obj==null || getClass()!=obj.getClass()) return false;
+        Contacts c=(Contacts) obj;
+        return f_name.equals(c.f_name) &&
+                l_name.equals(c.l_name);
     }
 
-    public void setFirstname(String next) {
-        this.f_name=next;
+
+    public int hashCode(){
+        return Objects.hash(f_name,l_name,address,city,state,zip,phn_no,email);
     }
 
-    public void setLastname(String next) {
-        this.l_name=next;
-    }
 
-    public void setCity(String next) {
-        this.city=next;
-    }
-
-    public void setState(String next) {
-        this.state=next;
-    }
-
-    public void setEmail(String next) {
-        this.email=next;
-    }
-
-    public void setPhone(int i) {
-        this.phn_no=i;
-    }
-
-    public void setZip(int i) {
-        this.zip=i;
-    }
-
-    public String toString() {
-        return "["+f_name+" "+l_name+" " +address+" "+city+" "+state+" "+zip+" "+phn_no+" "+email+"]";
-    }
 }
