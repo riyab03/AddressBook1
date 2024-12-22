db880fdd0fd99cc03a7bfad2b9bb0d7408c0b7d2
rbisht03 <riyabisht979@gmail.com>
2024-12-22 15:06:24 +0530

    Add multiple person

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to Address Book Program");
-
         AddressBook a1=new AddressBook();
-        int i=0;
-        a1.CreateContact();
-        a1.editContact();
-        a1.deleteContact();
-        a1.display();
+        int loop=1;
+        do {
+            System.out.println("Enter 1}creating a contact 2}editing the details 3}deleting contact detail 4}display the information");
+            int i = sc.nextInt();
+            switch (i) {
+                case 1->
+                    a1.CreateContact();
+                case 2->
+                    a1.editContact();
+                case 3->
+                    a1.deleteContact();
+                case 4->
+                    a1.display();
+                default -> {
+                    break;
+                }
+            }
+            System.out.println("Enter 0 to Exit or 1 to continue");
+            loop=sc.nextInt();
+        }while(loop!=0);
     }
 }
